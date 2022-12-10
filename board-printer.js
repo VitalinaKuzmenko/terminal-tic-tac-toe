72c5e74cb61ab73d3800bd7e71abc286962f84ed
Vitalina Kuzmenko <vitalina.kuzmenko.a@gmail.com>
2022-12-10 15:02:29 +0000

    Adding function printBoard(board) into board-printer.js

--- board-printer.js
+++ board-printer.js
@@ -15,7 +15,61 @@
         =================
     Test your function by calling it with an example tic-tac-toe board.
 */
+
 export function printBoard(board) {
+  let row1 = [];
+  let row2 = [];
+  let row3 = [];
+
+  for (let i = 0; i < board.length; i++) {
+    for (let j = 0; j < board[i].length; j++) {
+      let element = board[i][j];
+      if (i === 0) {
+        if (element === "X" || element === "O") {
+          row1.push(element);
+          if (j != 2) {
+            row1.push("|");
+          }
+        } else if (element === "_") {
+          row1.push(" ");
+          if (j != 2) {
+            row1.push("|");
+          }
+        }
+      } else if (i === 1) {
+        if (element === "X" || element === "O") {
+          row2.push(element);
+          if (j != 2) {
+            row2.push("|");
+          }
+        } else if (element === "_") {
+          row2.push(" ");
+          if (j != 2) {
+            row2.push("|");
+          }
+        }
+      } else if (i === 2) {
+        if (element === "X" || element === "O") {
+          row3.push(element);
+          if (j != 2) {
+            row3.push("|");
+          }
+        } else if (element === "_") {
+          row3.push(" ");
+          if (j != 2) {
+            row3.push("|");
+          }
+        }
+      }
+    }
+  }
+
+  console.log(row1.join(" "));
+  console.log("=================");
+  console.log(row2.join(" "));
+  console.log("=================");
+  console.log(row3.join(" "));
+  console.log("=================");
 }
 
 /*
@@ -23,5 +77,4 @@ export function printBoard(board) {
         - return true if there are no moves left to make (there are no more '_' values)
         - return false if there are still moves that can be made
 */
-export function checkIfNoMovesLeft(board) {
-}
+export function checkIfNoMovesLeft(board) {}
